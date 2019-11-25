956b19cb9d6a368f6edaefdf4d678f0daf52d10a
Donner Hanson <donnerhansoncomposer@yahoo.com>
2019-11-25 02:53:34 -0800

    Working Cars and My_old_cars input file

--- cars.sh
+++ cars.sh
@@ -2,14 +2,14 @@
 # cars.sh
 # Donner Hanson
 
-echo "enter 1 to enter a new car, 2 to display list of cars (sorted by year), or 3 to quit the program";
+echo "enter 1 to enter a new car, 2 to display list of cars (sorted by year), or 3 to quit the program:";
 option="0";
 
 while [ $option -ne 3 ]
 do
 read -r option;
 
-case $option in 
+case $option in
 	1)
 	# new car - input individually and store as string YR:MK:MOD
 	echo "enter year";
@@ -20,22 +20,33 @@ case $option in
 	read -r MDL;
 	DETAILS="$YR:$MK:$MDL";
 	# append to file My_old_cars
-	echo "$DETAILS" >> My_old_cars; 
-	# read info from file My_old_cars
-	# sort each line by year
-	echo "enter 1 to enter a new car, 2 to display list of cars (sorted by year), or 3 to quit";
+	echo "$DETAILS" >> My_old_cars;
+	# change any delimiting ':' to ' ' and redirect all
+	# output from command to null
+	# FS is the field separator and OFS is the output field separator
+	awk '$1=$1' FS=":" OFS=" " My_old_cars &> /dev/null;
+	# sort the list by the first column by numeric value
+	# and output to tempFile.txt
+	sort -nk1 My_old_cars > tempFile.txt;
+	# rename tempFile as My_old_cars
+	mv tempFile.txt My_old_cars;
+	# replace delimiting ':' in place of ' ' and redirect all command
+	# output to null
+	awk '$1=$1' FS=" " OFS=":" My_old_cars &> /dev/null;
+	echo "enter 1 to enter a new car, 2 to display list of cars (sorted by year), or 3 to quit:";
 	;;
 	2)
-	# display list sorted by year
-	echo "enter 1 to enter a new car, 2 to display list of cars (sorted by year), or 3 to quit"; 
+	# display previously sorted file
+	cat My_old_cars;
+	echo "enter 1 to enter a new car, 2 to display list of cars (sorted by year), or 3 to quit:";
 	;;
 	3)
 	#quit and exit
-	echo "exiting";
+	echo "Goodbye";
 	;;
 	*)
-	echo "not valid: enter a value in [1, 3]"
+	echo "not valid: enter a value in [1, 3]:"
 	;;
-	
+
 esac
 done
